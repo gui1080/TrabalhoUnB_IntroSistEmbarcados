89ba7d7366a8cccf2565ff2d292f139ffe18e006
gui1080 <fbguib@gmail.com>
2022-03-09 12:35:56 -0300

    dividido os canais de cores

--- app/src/main/java/com/example/sistemasembarcados/CameraActivity.java
+++ app/src/main/java/com/example/sistemasembarcados/CameraActivity.java
@@ -14,8 +14,10 @@ import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.JavaCameraView;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 
 import androidx.annotation.NonNull;
 import android.util.Log;
@@ -27,6 +29,9 @@ public class CameraActivity extends AppCompatActivity implements CameraBridgeVie
     Mat mRGBA;
     Mat mRGBAT;
     CameraBridgeViewBase cameraBridgeViewBase;
+
+    int counter = 0;
+
     BaseLoaderCallback baseLoaderCallback = new BaseLoaderCallback(this) {
 
         @Override
@@ -132,10 +137,44 @@ public class CameraActivity extends AppCompatActivity implements CameraBridgeVie
     // processamento frame a frame
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+
+        // frame colorido!
         mRGBA = inputFrame.rgba();
-        mRGBAT = inputFrame.gray();
+        //mRGBAT = inputFrame.gray();
+
+        // pega o frame no frame 10, 20 e 30, a cada 10 frames!
+        if( (counter % 48) == 0){
+
+            //Core.flip(frame, frame, 90);
+
+            int rows = mRGBA.rows();
+            int cols = mRGBA.cols();
+            // int ch = mRgba.channels();
+            double R=0,G=0,B=0;
+
+            for (int i=0; i<rows; i++)
+            {
+                for (int j=0; j<cols; j++)
+                {
+                    double[] data = mRGBA.get(i, j); //Stores element in an array
+
+                    // acessando valor de red/green/blue frame a frame
+                    // salvar em outra matriz
+
+                    R = data[0];
+                    G= data[1];
+                    B = data[2];
+                }
+            }
 
 
+        }
+
+        counter = counter + 1;
+
+        if(counter == 49){
+            counter = 0;
+        }
 
         return mRGBA;
     }
